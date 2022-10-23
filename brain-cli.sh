71615021a42fe16f9c0eb8694b295948367d0668
octoshrimpy <octavo.acapella@gmail.com>
2022-10-23 17:28:33 -0600

    thinking through sh

--- brain-cli.sh
+++ brain-cli.sh
@@ -3,4 +3,34 @@
 
 # https://google.github.io/styleguide/shellguide.html
 
-while getopts ""
+
+####### opening without args
+
+# set openDir = os.pwd()
+
+# if not within git repo
+# prompt user for git init
+
+# if not readme.md
+# prompt user for "is this root of brain, where books will be stored?"
+# if yes
+# create readme with "root" in frontmatter
+
+
+# files;
+# folders;
+
+# if no files && no folders
+# tell user no notes
+# hint user at new note/book creation
+
+# for file
+# parse frontmatter
+# display file
+
+# for folder
+# parse folder/readme.md (insensitive)
+# display book
+
+# if file is notetype todo
+# parse todo and show in sidebar
