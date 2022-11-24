c141cdc2de057c986ebfb7e82188fa1cbb085268
octoshrimpy <octavo.acapella@gmail.com>
2022-11-23 23:12:20 -0700

    omnibar borders and bit of styling

--- null/layout.js
+++ null/layout.js
@@ -10,7 +10,137 @@ var   StateMachine = require( 'text-machine' )
 
 var term = termkit.terminal
 
-let app = {}
+let app = {
+//@todo figure out better logic for these methods and properties
+quit: () => {
+  term.grabInput( false )
+  term.hideCursor( false )
+  term.styleReset()
+  term.clear()
+  process.exit()
+},
+
+  /**
+   * open a file/path
+   * 
+   * @param {string} filepath - path to the file to open
+   * 
+   * @memberof app
+   * 
+   * @todo handle folder path too
+   */
+   open: async (args) => {
+
+    let dirpath = __dirname
+    let filePath
+    let fileContent = ""
+    let fileLang = "javascript"
+
+
+    app.createTree(dirpath)
+    
+    if (args.length == 0) {
+      fileContent = ""
+    } else {
+      filePath = args.toString()
+      fileLang = getLang(filePath)
+      // console.log(fileLang)
+      let pathIsFile = isFile(filePath)
+      
+      if (!pathIsFile) {
+        fileContent = `${filePath} is not a file!`
+      } else {
+        fileContent = fs.readFileSync(filePath, 'utf8')
+      }
+    }
+    
+    var stateMachine = new StateMachine( {
+      program: require( `text-machine/languages/${fileLang}.js` ) ,
+      api: termkit.TextBuffer.TextMachineApi
+    } )
+
+
+    // console.log(fileContent)
+    // editor.stateMachine = stateMachine
+    editor.setContent(fileContent)
+    editor.filePath = filePath
+
+    document.giveFocusTo( editor )
+    editor.textBuffer.moveTo(0,0)
+  },
+
+  //@fixme document this
+  toggleTree: () => {
+    let elm = layout.layoutDef.columns.filter(col => col.id == 'sidebarContainer')[0]
+    
+    if (app.sidebar.isOpen) {
+      while(elm.width > app.sidebar.widthClosed) {
+        elm.width -= 1
+        layout.computeBoundingBoxes()
+        layout.redraw()
+      }
+      
+    } else {
+      app.sidebar.openPane = "tree"
+      // app.sidebar.setContent(app.tree)
+  
+      while(elm.width < app.sidebar.widthOpen) {
+        elm.width += 1
+        layout.computeBoundingBoxes()
+        layout.redraw()
+      }
+    }
+
+    layout.computeBoundingBoxes()
+    layout.redraw()
+    app.sidebar.isOpen = !app.sidebar.isOpen
+  },
+
+  //@fixme document this
+  createTree: (pathStr = __dirname) => {
+
+    //@todo extract this into settings
+    let options = {
+      excludeHidden: true,
+      foldersFirst: true
+    }
+  
+    let treeItems = path2obj(pathStr)
+    app.tree = new termkit.ColumnMenu({
+      width: 100,
+      parent: document.elements.sidebar,
+      items: treeItems,
+      // disposition: 'inline'
+      //@todo should this be a `this`?
+    }).on('focus', app.onTreeFocus)
+  },
+
+  //@fixme document this
+  onTreeFocus:(buttonValue, action) => {
+
+    let item = app.tree.getItem( buttonValue )
+    
+    if (item.type == "folder") {
+      // app.tree.setItem(item, {content: "hello world"})
+      // console.log(item)
+  
+      //@todo implement this custom tree collapse menu
+      // if folder state is closed
+        // get index of folder clicked in app.tree
+        // get children that should show
+        // get indentation level from folder clicked
+        // add new children to app.tree, with left indentation
+        // set folder state to open
+      // else
+        // for every item after folder that has a bigger indentation number
+          // item.destroy
+        // set folder state to closed
+    } else
+    if (item.type == "file") {
+      app.content.setContent()
+    }
+  }
+}
 
 app.filePath
 
@@ -98,7 +228,7 @@ term.on('mouse', (name, opts) => {
 })
 
 
-term.on( 'key' , function( key ) {
+term.on( 'key' , async function( key ) {
 	switch(key)
   {
     case 'CTRL_C':
@@ -111,7 +241,36 @@ term.on( 'key' , function( key ) {
     
     case 'CTRL_E':
       toggleTree()
+      break    
+      
+    //@todo extract this logic into `app.save(content)`
+    case 'CTRL_S' : 
+      let content = editor.getContent()
+
+      // not opened with a path, doesn't exist yet
+      if (!editor.filePath) {
+
+        // ask for namepath
+        let prompt = "enter filepath: "
+        let placeholder = "save/to/path"
+
+        // create floating input
+        editor.filePath = await createInput(prompt, placeholder)
+
+      }
+
+      // in case we cancelled the prompt and it returned empty 
+      if (editor.filePath) {
+
+        // write to path given
+        writeToFile(editor.filePath, content)
+      }
       break
+  
+      // https://github.com/cronvel/terminal-kit/blob/master/lib/vte/toInputSequence.js#L173
+      // grabbing 'CTRL_SPACE'
+      case 'NUL' :
+        await omni()
     
     case 'CTRL_G':
       // debug
@@ -287,7 +446,7 @@ editor.open = async (args) => {
     fileContent = ""
   } else {
     filePath = args.toString()
-    // fileLang = termkit.fileHelpers.getLang(filePath)
+    // fileLang = getLang(filePath)
     fileLang = "javascript"
     let pathIsFile = fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()
     
@@ -305,7 +464,9 @@ editor.open = async (args) => {
 
 
   // console.log(fileContent)
-  editor.stateMachine = stateMachine
+
+  //@fixme can't change statemachine
+  // editor.stateMachine = stateMachine
   editor.setContent(fileContent)
   editor.filePath = filePath
 
@@ -313,6 +474,249 @@ editor.open = async (args) => {
   editor.textBuffer.moveTo(0,0)
 }
 
+
+
+
+app.cmdHistory = []
+app.cmds = {
+  quit: (args) => app.quit(),
+  open: async (args) => {
+    let opts = { baseDir: './', autoCompleteMenu: true, autoCompleteHint: true }
+    let filePath = await createInput("path: ", "", termkit.InlineFileInput, opts)
+    filePath && await app.open(filePath)
+  },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * writes given contents into given 
+ *          filepath, whether it exists or not
+ * 
+ * @param {string} filePath - path to file to write, does not have to exist
+ * @param {string} contents - plaintext to write to file
+ * 
+ * @todo alert user that file was saved
+ */
+ function writeToFile(filePath, contents, cb = () => {}) {
+  fs.mkdir(path.dirname(filePath), { recursive: true}, function (err) {
+    if (err) return cb(err);
+    
+    fs.writeFile(filePath, contents, cb);
+  });
+}
+
+
+
+/**
+ * opens omnibar
+ * 
+ * @todo load available commands into auto-complete
+ * @done set files in current open directory as autocomplete
+ */
+ async function omni() {
+  let cmd = await createInput()
+  
+  cmd != null && await cmdHandler(cmd)
+}
+
+/**
+ * does the logic for the given command
+ * 
+ * @param {string} cmdstr - command to run
+ * 
+ * @todo each command should be extracted into its own file
+ * @todo iter over the commands directory in settings
+ */
+async function cmdHandler(cmdStr) {
+  
+  app.cmdHistory.push(cmdStr)
+
+  let cmd = cmdStr.split(" ").shift()
+
+  if (cmd in app.cmds) {
+    return app.cmds[cmd].call(this, cmdStr)
+  }
+  return 
+
+  switch(cmd)
+  {
+    case "quit" :
+      app.quit()
+      break;
+    
+    case "open" : 
+      //@todo set basedir as the dir of currently open file
+      let opts = { baseDir: './', autoCompleteMenu: true, autoCompleteHint: true }
+      let filePath = await createInput("path: ", "", termkit.InlineFileInput, opts)
+      await app.open(filePath)
+      break
+  }
+}
+
+/**
+ * creates floating input used in any popup menu
+ * 
+ * @param {string} prompt - inline label for the input
+ * @param {string} placeholder - placeholder text for input
+ * @param {termkit-input} type - LabeledInput, InlineInput, InlineFileInput
+ * @param {Object} [opts = {}] - optional options to pass to the input
+ * 
+ * @returns {Promise} promise represents resulting content from input upon input close
+ * 
+ * @todo figure out better way to handle all these options
+ */
+async function createInput(prompt = '', placeholder = '', type = null, opts = {}) {
+
+  let inputType = type != null ? type : termkit.InlineInput;
+  
+  //@todo extract position into opts/settings so user can decide where it opens
+  let x = (term.width / 2) - 25 - 4
+  
+  // automatically build the command autocomplete system
+  let cmdAutocomplete = []
+  Object.keys(app.cmds).forEach(cmd => cmdAutocomplete.push(cmd))
+
+  // setup params and init
+  var input = new (inputType)( {
+    ...opts,
+    parent: document ,
+    // textAttr: { bgColor: 'blue' } ,
+    // voidAttr: { bgColor: 'blue' } ,
+    placeholder: placeholder ,
+    x: x ,
+    y: 10 ,
+    prompt: {
+      // textAttr: { bgColor: 'blue' } ,
+      content: prompt,
+      contentHasMarkup: true
+    } ,
+    width: 50 ,
+    cancelable: true,
+    autoCompleteMenu: true,
+    autoCompleteHint: true,
+    history: app.cmdHistory,
+    autoComplete: cmdAutocomplete,
+    menu: {
+      y: 2,
+      buttonBlurAttr: { bgColor: 'default' , color: 'default' } ,
+      buttonFocusAttr: { bgColor: 'green' , color: 'black'} ,
+      buttonDisabledAttr: { bgColor: 'white' , color: 'brightBlack' } ,
+      buttonSubmittedAttr: { bgColor: 'brightWhite' , color: 'brightBlack' } ,
+      buttonSeparatorAttr: { bgColor: 'default' } ,
+      backgroundAttr: { bgColor: 'default' } ,
+      //leftPadding: ' ' , rightPadding: ' ' ,
+      justify: true ,
+      keyBindings: Object.assign( {} , termkit.ColumnMenu.prototype.keyBindings , {
+        TAB: 'next' ,
+        SHIFT_TAB: 'previous'
+      })
+    }
+  } )
+
+  var border = new termkit.Border({
+    parent: input,
+    attr: {bgColor: ''},
+    frameChars: 'lightRounded'
+  })
+
+  document.giveFocusTo(input) /// focus input
+  
+  //@todo terminalkit has promises built-in. 
+  // figure it out, and replace this monstrosity
+  return new Promise((resolve, rej) => {
+
+    // create a cancel event in here,
+    // as the inputs created will always be the same
+    function onCancel() {
+      term.saveCursor()
+      input.destroy()
+      document.focusNext()
+      term.restoreCursor()
+
+      resolve(null)
+    }
+
+    function onSubmit() {
+      term.saveCursor()
+      input.destroy()
+      document.focusNext()
+      term.restoreCursor()
+
+      resolve(input.getContent())
+    }
+
+    input.on( 'cancel' , onCancel )
+    input.on('submit', onSubmit)
+
+    //@todo on click outside the omni, cancel
+  })
+}
+
+  /**
+   * returns true if given path is a file
+   * 
+   * @param {string} filepath 
+   * @returns boolean file is file
+   */
+  let isFile = (filePath) => {  
+    return fs.existsSync(filePath) && fs.lstatSync(filePath).isFile();
+  }
+  
+  /**
+   * gets proper extension for given file, ie: `.tar.gz`, instead of just `.gz`
+   * 
+   * @source https://stackoverflow.com/a/15778396
+   * @param {string} filepath
+   * @returns {string} file extension starting with `.`, ie: `.md`
+   * 
+   * @todo check if it's a file before attempting anything
+   */
+  let getExt =(filePath) => {
+    return filePath
+    .split('.')
+    .filter(Boolean) // removes empty extensions (e.g. `filename...txt`)
+    .slice(1)
+    .join('.')
+  }
+
+  /**
+   * gets text-machine language for given file
+   * 
+   * @param {string} filepath
+   * @returns {string} text-machine language
+   */
+  let getLang =(filePath) => {
+    let ext = getExt(filePath)
+    
+    //@todo set default as plaintext
+    switch(ext)
+    {
+      case "css":
+        return "css";
+        
+      case "js":
+        return "javascript";
+      
+      case "vue":
+        return "vue";
+          
+      case "xml":
+        return "xml";
+        
+      default:
+        return "javascript";
+    }
+  }
 
 
 
